ca02bd79b8c32db6bf3fb4000b3528f9b24fd55f
Júlia R. Walter <64220340+juliarwalter@users.noreply.github.com>
2022-12-13 17:44:09 -0300

    Add ProjetoFintech
    Add "ProjetoFintech" + "css" + "img" + "html" + "js" file

---
+++ ProjetoFintech/js/home.js
@@ -0,0 +1,6 @@
+const botaoMenu = document.querySelector(".menu")
+const menu = document.querySelector("nav.menu_lateral")
+
+botaoMenu,addEventListener(click, () =>{
+    menu.classList.toggle("nav.menu_lateral--active")
+})
